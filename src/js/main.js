56be5334f6cf97489a8da425efd63300dbf4216a
Alchemist of the web <nikitakzntcv@gmail.com>
2024-07-18 16:50:30 +0400

    add modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // const sum = require("./module/sum.js");
 import AOS from "aos";
-
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
 document.addEventListener('DOMContentLoaded', () => {
     console.log("using wow1");
     new AOS.init({
